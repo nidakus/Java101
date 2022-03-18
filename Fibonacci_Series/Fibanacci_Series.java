f9eecd830d894254a5ea53671f79962349d88af9
Nida <85497008+nidakus@users.noreply.github.com>
2022-03-19 00:06:35 +0300

    Add files via upload

---
+++ Fibonacci_Series/Fibanacci_Series.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Fibanacci_Series {
+
+    public static void main(String[] args) {
+
+        Scanner inp = new Scanner(System.in);
+        System.out.print("Enter the number of the Fibonacci series: ");
+        int number = inp.nextInt();
+        int n1 = 0, n2 = 1;
+        System.out.print(n1 + " "+ n2);
+
+        for(int i=1; i<=number; i++){
+
+            int  sum = n1 + n2;
+            System.out.print(" "+ sum);
+            n1 = n2;
+            n2 = sum;
+
+        }
+    }
+}
