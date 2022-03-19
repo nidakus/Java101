021425048abc91c5e65fb1e017757e348218c7b1
Nida Kus <85497008+nidakus@users.noreply.github.com>
2022-03-19 12:48:25 +0300

    www.patika.dev

--- Fibonacci_Series/Fibanacci_Series.java
+++ Fibonacci_Series/Fibanacci_Series.java
@@ -8,15 +8,13 @@ public class Fibanacci_Series {
         System.out.print("Enter the number of the Fibonacci series: ");
         int number = inp.nextInt();
         int n1 = 0, n2 = 1;
-        System.out.print(n1 + " "+ n2);
 
         for(int i=1; i<=number; i++){
 
             int  sum = n1 + n2;
-            System.out.print(" "+ sum);
+            System.out.print(n1 + " + " + n2 + " = "+ sum);
             n1 = n2;
             n2 = sum;
-
         }
     }
 }
